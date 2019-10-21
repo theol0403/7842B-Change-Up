aed81e9246dc4d7f7476bd88aa7bc0e060c07145
theol0403 <theol0403@gmail.com>
2019-10-20 23:19:07 -0700

    add ascii art

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -29,6 +29,16 @@ std::shared_ptr<ThreeEncoderOdometry> Robot::getOdom() {
   return instance.odom;
 }
 
+/***
+ *     _____ _                   _     
+ *    /  __ \ |                 (_)    
+ *    | /  \/ |__   __ _ ___ ___ _ ___ 
+ *    | |   | '_ \ / _` / __/ __| / __|
+ *    | \__/\ | | | (_| \__ \__ \ \__ \
+ *     \____/_| |_|\__,_|___/___/_|___/
+ *                                     
+ *                                     
+ */
 void Robot::initializeChassis() {
   model = std::make_shared<ThreeEncXDriveModel>(
     // motors
@@ -47,8 +57,28 @@ void Robot::initializeChassis() {
     TimeUtilFactory::create(), model, ChassisScales({2.75_in, 13.2_in, 0.001_in}, 360));
 }
 
+/***
+ *    ______           _               
+ *    |  _  \         (_)              
+ *    | | | |_____   ___  ___ ___  ___ 
+ *    | | | / _ \ \ / / |/ __/ _ \/ __|
+ *    | |/ /  __/\ V /| | (_|  __/\__ \
+ *    |___/ \___| \_/ |_|\___\___||___/
+ *                                     
+ *                                     
+ */
 void Robot::initializeDevices() {}
 
+/***
+ *     _____                          
+ *    /  ___|                         
+ *    \ `--.  ___ _ __ ___  ___ _ __  
+ *     `--. \/ __| '__/ _ \/ _ \ '_ \ 
+ *    /\__/ / (__| | |  __/  __/ | | |
+ *    \____/ \___|_|  \___|\___|_| |_|
+ *                                    
+ *                                    
+ */
 void Robot::initializeScreen() {
   odomScreen = std::make_shared<OdomDebug>(lv_scr_act(), LV_COLOR_ORANGE);
 
