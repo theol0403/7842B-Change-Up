5ce9116b2e87352a3b54f1c6dcf67a50855b8716
Theo Lemay <theol0403@gmail.com>
2019-11-10 16:30:04 -0800

    comment statemachine

--- include/7842Fmain/util/statemachine.hpp
+++ include/7842Fmain/util/statemachine.hpp
@@ -1,6 +1,13 @@
 #pragma once
 #include "main.h"
 
+/**
+ * State machine helper class.
+ *
+ * @tparam States       An enum class representing the states of a subsystem. Required to have an off state.
+ * @tparam assumedState Optional - The assumed last state when using setNewState. Initially calling setNewState with
+ *                      this state will not trigger a state transition.
+ */
 template <typename States, States assumedState = States::off>
 class StateMachine : public TaskWrapper {
 
@@ -8,10 +15,20 @@ class StateMachine : public TaskWrapper {
   StateMachine() = default;
   virtual ~StateMachine() = default;
 
+  /**
+   * Sets the state.
+   *
+   * @param istate The state
+   */
   virtual void setState(const States& istate) {
     state = istate;
   }
 
+  /**
+   * Sets the state only if the state is different from the last time this function was called.
+   *
+   * @param istate The state
+   */
   virtual void setNewState(const States& istate) {
     if (istate != lastState) {
       state = istate;
@@ -19,13 +36,25 @@ class StateMachine : public TaskWrapper {
     }
   }
 
+  /**
+   * Gets the state.
+   *
+   * @return The state.
+   */
   virtual const States& getState() const {
     return state;
   }
 
  protected:
+  /**
+   * Override this method to implement setup procedures.
+   */
   virtual void calibrate() = 0;
-  virtual void loop() = 0;
+
+  /**
+   * Override this method to implement the statemachine task
+   */
+  void loop() override = 0;
 
   States state {States::off};
   States lastState {assumedState};
