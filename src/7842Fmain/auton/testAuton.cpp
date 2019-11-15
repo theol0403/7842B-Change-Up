6103b0c5552585d0e75c7f4c571c754cdb91fa51
Theo Lemay <theol0403@gmail.com>
2019-11-14 17:25:53 -0800

    auton work

--- src/7842Fmain/auton/testAuton.cpp
+++ src/7842Fmain/auton/testAuton.cpp
@@ -2,39 +2,42 @@
 
 void testAuton() {
   auto&& chassis = *Robot::chassis();
+  Robot::odom()->setState({11_ft, 5_ft, -90_deg});
 
   // deploy lift
   pros::Task deploy(
     [](void*) {
       Robot::deploy();
+      Robot::lift()->setPosition({0, 0});
+      Robot::lift()->setState(liftStates::holdAtPos);
+
+      Robot::clawLeft()->setState(clawStates::release);
+      Robot::clawRight()->setState(clawStates::release);
     },
     nullptr, "deploy");
 
   chassis.strafeToPoint(
-    {4_ft, 4.5_ft},
+    {8_ft, 4_ft},
     [](const OdomController& odom) {
-      if (odom.distanceToPoint({4_ft, 4.5_ft}) < 2_ft) {
-        return lib7842::OdomMath::rollAngle180(-90_deg - Robot::odom()->getState().theta);
+      if (odom.distanceToPoint({8_ft, 4_ft}) < 2_ft) {
+        return lib7842::OdomMath::rollAngle180(0_deg - Robot::odom()->getState().theta);
       } else {
         return 0_deg;
       }
     },
     1, OdomController::makeSettler(6_in));
 
-  Robot::clawLeft()->setState(clawStates::release);
-  Robot::clawRight()->setState(clawStates::release);
-
   chassis.strafeToPoint(
-    {4_ft, 5.5_ft}, OdomController::makeAngleCalculator({4_in, 7_in}), 1,
+    {8_ft, 5_ft}, OdomController::makeAngleCalculator(0_deg), 1,
     OdomController::defaultDriveAngleSettler);
 
   Robot::clawLeft()->setState(clawStates::clamp);
   Robot::clawRight()->setState(clawStates::clamp);
-  pros::delay(300);
-
-  Robot::lift()->setPosition({100, 100});
-  Robot::lift()->setState(liftStates::hold);
   pros::delay(500);
 
-  chassis.strafeToPoint({2.5_ft, 7.5_ft}, OdomController::makeAngleCalculator(-90_deg), 1);
+  Robot::lift()->setPosition({300, 300});
+  Robot::lift()->setState(liftStates::holdAtPos);
+  pros::delay(500);
+
+  chassis.strafeToPoint({9.5_ft, 7.5_ft}, OdomController::makeAngleCalculator(0_deg), 1);
 }
