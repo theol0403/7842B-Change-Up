7e971337cf2aae152c3d5614232300bad763cd79
Theo Lemay <theol0403@gmail.com>
2019-12-23 15:02:35 -0800

    disable claw

--- include/7842Fmain/subsystem/claw.hpp
+++ include/7842Fmain/subsystem/claw.hpp
@@ -1,35 +1,35 @@
-#pragma once
-#include "main.h"
-#include "7842Fmain/util/statemachine.hpp"
+// #pragma once
+// #include "main.h"
+// #include "7842Fmain/util/statemachine.hpp"
 
-enum class clawStates {
-  off, // motor 0 voltage
-  close, // motor 100% voltage
-  open, // motor -100% voltage
-  clamp, // pid to close position
-  release, // pid to open position
-  brake, // apply motor brake
-  calibrate, // apply motor brake
-  hold,
-  holdAtPos,
-  deploy
-};
+// enum class clawStates {
+//   off, // motor 0 voltage
+//   close, // motor 100% voltage
+//   open, // motor -100% voltage
+//   clamp, // pid to close position
+//   release, // pid to open position
+//   brake, // apply motor brake
+//   calibrate, // apply motor brake
+//   hold,
+//   holdAtPos,
+//   deploy
+// };
 
-class Claw : public StateMachine<clawStates, clawStates::hold> {
+// class Claw : public StateMachine<clawStates, clawStates::hold> {
 
- public:
-  Claw(const std::shared_ptr<Motor>& iclaw, const std::shared_ptr<IterativePosPIDController>& ipid);
+//  public:
+//   Claw(const std::shared_ptr<Motor>& iclaw, const std::shared_ptr<IterativePosPIDController>& ipid);
 
-  std::shared_ptr<Motor> getMotor() const;
-  double getPosition();
+//   std::shared_ptr<Motor> getMotor() const;
+//   double getPosition();
 
- protected:
-  void initialize() override;
-  void loop() override;
+//  protected:
+//   void initialize() override;
+//   void loop() override;
 
-  std::shared_ptr<Motor> claw {nullptr};
-  std::shared_ptr<IterativePosPIDController> pid {nullptr};
+//   std::shared_ptr<Motor> claw {nullptr};
+//   std::shared_ptr<IterativePosPIDController> pid {nullptr};
 
-  double startPos {0};
-  double holdPos {0};
-};
+//   double startPos {0};
+//   double holdPos {0};
+// };
--- src/7842Fmain/subsystem/claw.cpp
+++ src/7842Fmain/subsystem/claw.cpp
@@ -1,78 +1,78 @@
-#include "claw.hpp"
+// #include "claw.hpp"
 
-Claw::Claw(
-  const std::shared_ptr<Motor>& iclaw, const std::shared_ptr<IterativePosPIDController>& ipid) :
-  claw(std::move(iclaw)), pid(std::move(ipid)) {
-  initialize();
-  startTask("claw");
-}
+// Claw::Claw(
+//   const std::shared_ptr<Motor>& iclaw, const std::shared_ptr<IterativePosPIDController>& ipid) :
+//   claw(std::move(iclaw)), pid(std::move(ipid)) {
+//   initialize();
+//   startTask("claw");
+// }
 
-std::shared_ptr<Motor> Claw::getMotor() const {
-  return claw;
-}
+// std::shared_ptr<Motor> Claw::getMotor() const {
+//   return claw;
+// }
 
-void Claw::initialize() {
-  claw->setBrakeMode(AbstractMotor::brakeMode::brake);
-  claw->setEncoderUnits(AbstractMotor::encoderUnits::degrees);
+// void Claw::initialize() {
+//   claw->setBrakeMode(AbstractMotor::brakeMode::brake);
+//   claw->setEncoderUnits(AbstractMotor::encoderUnits::degrees);
 
-  startPos = claw->getPosition();
-}
+//   startPos = claw->getPosition();
+// }
 
-double Claw::getPosition() {
-  return claw->getPosition() - startPos;
-}
+// double Claw::getPosition() {
+//   return claw->getPosition() - startPos;
+// }
 
-void Claw::loop() {
+// void Claw::loop() {
 
-  while (true) {
+//   while (true) {
 
-    switch (state) {
+//     switch (state) {
 
-      case clawStates::off: claw->moveVoltage(0); break;
+//       case clawStates::off: claw->moveVoltage(0); break;
 
-      case clawStates::close: claw->moveVoltage(12000); break;
+//       case clawStates::close: claw->moveVoltage(12000); break;
 
-      case clawStates::open: claw->moveVoltage(-12000); break;
+//       case clawStates::open: claw->moveVoltage(-12000); break;
 
-      case clawStates::clamp:
-        pid->setTarget(-85);
-        claw->moveVoltage(pid->step(getPosition()) * 12000);
-        break;
+//       case clawStates::clamp:
+//         pid->setTarget(-85);
+//         claw->moveVoltage(pid->step(getPosition()) * 12000);
+//         break;
 
-      case clawStates::release:
-        pid->setTarget(-340);
-        claw->moveVoltage(pid->step(getPosition()) * 6000);
-        break;
+//       case clawStates::release:
+//         pid->setTarget(-340);
+//         claw->moveVoltage(pid->step(getPosition()) * 6000);
+//         break;
 
-      case clawStates::brake: claw->moveVelocity(0); break;
+//       case clawStates::brake: claw->moveVelocity(0); break;
 
-      case clawStates::calibrate:
-        do {
-          claw->moveVoltage(8000);
-          pros::delay(100);
-        } while (claw->getActualVelocity() > 8);
-        pros::delay(400);
-        startPos = claw->getPosition();
-        state = clawStates::clamp;
-        break;
+//       case clawStates::calibrate:
+//         do {
+//           claw->moveVoltage(8000);
+//           pros::delay(100);
+//         } while (claw->getActualVelocity() > 8);
+//         pros::delay(400);
+//         startPos = claw->getPosition();
+//         state = clawStates::clamp;
+//         break;
 
-      case clawStates::hold:
-        holdPos = getPosition();
-        state = clawStates::holdAtPos;
-        break;
+//       case clawStates::hold:
+//         holdPos = getPosition();
+//         state = clawStates::holdAtPos;
+//         break;
 
-      case clawStates::holdAtPos:
-        pid->setTarget(holdPos);
-        claw->moveVoltage(pid->step(getPosition()) * 12000);
-        break;
+//       case clawStates::holdAtPos:
+//         pid->setTarget(holdPos);
+//         claw->moveVoltage(pid->step(getPosition()) * 12000);
+//         break;
 
-      case clawStates::deploy:
-        holdPos = -100;
-        state = clawStates::holdAtPos;
-        break;
-    }
+//       case clawStates::deploy:
+//         holdPos = -100;
+//         state = clawStates::holdAtPos;
+//         break;
+//     }
 
-    // std::cout << "claw: " << claw->getPosition() << std::endl;
-    pros::delay(10);
-  }
-}
+//     // std::cout << "claw: " << claw->getPosition() << std::endl;
+//     pros::delay(10);
+//   }
+// }
