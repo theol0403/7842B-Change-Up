f2515d215dc56978c65991492c4610717e2c861b
Theo Lemay <theol0403@gmail.com>
2020-01-07 22:58:09 -0800

    Add points to odomHelper

--- include/7842Fmain/util/odomHelper.hpp
+++ include/7842Fmain/util/odomHelper.hpp
@@ -1,9 +1,34 @@
 #include "main.h"
 
 /**
+ * Constants
+ */
+extern const QLength clawOffset;
+
+extern const QLength cubeWidth;
+extern const QLength fieldWidth;
+extern const QLength towerBaseWidth;
+extern const QLength zoneWidth;
+extern const QLength bigZoneLength;
+extern const QLength barrierWidth;
+
+extern const Vector closeTower;
+extern const Vector leftTower;
+extern const Vector middleTower;
+extern const Vector rightTower;
+
+/**
  * Calculate position of robot for claw to be at given position and angle
  *
  * @param  state The state
  * @return The robot position
  */
 Vector toClaw(const State& state);
+
+/**
+ * Mirror the given coordinates across the field
+ */
+QAngle mirror(const QAngle& angle);
+QLength mirror(const QLength& y);
+Vector mirror(const Vector& point);
+State mirror(const State& state);
--- src/7842Fmain/util/odomHelper.cpp
+++ src/7842Fmain/util/odomHelper.cpp
@@ -1,9 +1,39 @@
 #include "odomHelper.hpp"
 
-QLength clawOffset = 0.5_ft; // TODO: adjust claw offset
+using namespace lib7842::units;
+
+const QLength clawOffset {1_ft}; // TODO: adjust claw offset
+
+const QLength cubeWidth {5.5_in};
+const QLength fieldWidth {6_tile};
+const QLength towerBaseWidth {7.8_in};
+const QLength zoneWidth {10_in};
+const QLength bigZoneLength {15.5_in};
+const QLength barrierWidth {2_in};
+
+const Vector closeTower = {3 * tile, tile};
+const Vector leftTower = {1.5 * tile, 3 * tile};
+const Vector middleTower = {3 * tile, 3 * tile};
+const Vector rightTower = {4.5 * tile, 3 * tile};
 
 Vector toClaw(const State& state) {
   return Vector(state)
          - Vector {std::sin(state.theta.convert(radian)) * clawOffset,
                    std::cos(state.theta.convert(radian)) * clawOffset};
+}
+
+QAngle mirror(const QAngle& angle) {
+  return angle * -1;
+}
+
+QLength mirror(const QLength& y) {
+  return 12_ft - y;
+}
+
+Vector mirror(const Vector& point) {
+  return {point.x, mirror(point.y)};
+}
+
+State mirror(const State& state) {
+  return {state.x, mirror(state.y), mirror(state.theta)};
 }
