131d9da12326afddc4e81e917c0f00e2ef974057
Theo Lemay <theol0403@gmail.com>
2020-01-08 21:12:16 -0800

    enable auton button

--- src/7842Fmain/driver.cpp
+++ src/7842Fmain/driver.cpp
@@ -29,7 +29,7 @@ void driverBaseControl() {
                           std::pow(rightY, 2) * util::sgn(rightY),
                           std::pow(leftX, 2) * util::sgn(leftX));
 
-  // if (mDigital(X) && !pros::competition::is_connected()) autonomous();
+  if (mDigital(X) && !pros::competition::is_connected()) autonomous();
 }
 
 /***
