8843fab97e761c9c7a4824eb828d42b1fb46edc7
Theo Lemay <theol0403@gmail.com>
2020-01-15 18:36:34 -0800

    set brake mode

--- src/7842Fmain/subsystem/claw.cpp
+++ src/7842Fmain/subsystem/claw.cpp
@@ -22,15 +22,24 @@ void Claw::loop() {
 
     switch (state) {
 
-      case clawStates::off: claw->moveVoltage(0); break;
+      case clawStates::off:
+        claw->setBrakeMode(AbstractMotor::brakeMode::brake);
+        claw->moveVoltage(0);
+        break;
 
-      case clawStates::brake: claw->moveVelocity(0); break;
+      case clawStates::brake:
+        claw->setBrakeMode(AbstractMotor::brakeMode::brake);
+        claw->moveVelocity(0);
+        break;
 
       case clawStates::close: claw->moveVoltage(12000); break;
 
       case clawStates::open: claw->moveVoltage(-12000); break;
 
-      case clawStates::clamp: claw->moveVoltage(0); break;
+      case clawStates::clamp:
+        claw->setBrakeMode(AbstractMotor::brakeMode::coast);
+        claw->moveVoltage(0);
+        break;
     }
 
     rate.delayUntil(10);
