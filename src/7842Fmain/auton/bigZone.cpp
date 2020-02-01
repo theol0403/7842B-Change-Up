42f923592e64abe53911591472e8d73f79c277bd
Theo Lemay <theol0403@gmail.com>
2020-01-31 19:06:50 -0800

    auton work

--- src/7842Fmain/auton/bigZone.cpp
+++ src/7842Fmain/auton/bigZone.cpp
@@ -4,10 +4,10 @@ void bigPreloadProtected(const std::shared_ptr<SideController>& controller) {
   auto&& [chassis, side] = getChassis();
 
   // TODO: measure position
-  Robot::odom()->setState(mirror({9_in, 9.8_ft, 90_deg}, side));
+  Robot::odom()->setState(mirror({8.95_in, 9.75_ft, 90_deg}, side));
 
   // drive forward a bit
-  chassis.strafeAbsoluteDirection(4_in, 100_deg, makeAngle(90_deg), 1, makeSettle(2_in));
+  chassis.strafeAbsoluteDirection(4.5_in, 100_deg, makeAngle(90_deg), 1, makeSettle(2_in));
 
   // push cube into goal
   chassis.strafeAbsoluteDirection(9_in, -10_deg, makeAngle(90_deg), 1, makeSettle(2_in));
@@ -19,12 +19,14 @@ void bigPreloadProtected(const std::shared_ptr<SideController>& controller) {
   asyncTask(Robot::get().deploy(); Robot::claw()->setState(clawStates::close););
 
   // wait for robot to deploy
-  pros::delay(1000);
+  pros::delay(500);
+  chassis.strafeAbsoluteDirection(3_in, 100_deg, makeAngle(90_deg), 1, makeSettle(2_in));
+  pros::delay(600);
 
   // grab inner protected cube
   spikeCube();
   Robot::claw()->setState(clawStates::clamp);
-  Robot::lift()->goToPosition(700);
+  Robot::lift()->goToPosition(Lift::aboveCubePos);
 
   // drive to outer cube
   chassis.strafeToPoint(toClaw({outerProtectedCube, 90_deg}), makeAngle(90_deg));
@@ -46,26 +48,41 @@ void bigGrabStack(const std::shared_ptr<SideController>& controller) {
   speedUp();
 
   // spike stack and raise lift a bit
-  spikeCube();
-  Robot::lift()->goToPosition(Lift::aboveCubePos + 50);
+
+  Robot::lift()->setState(liftStates::down);
+  while (Robot::lift()->getHeight() > 500) {
+    pros::delay(10);
+  }
+  Robot::lift()->setState(liftStates::up);
+  pros::delay(400);
+
+  Robot::lift()->setState(liftStates::down);
+  while (Robot::lift()->getHeight() > 60) {
+    pros::delay(10);
+  }
+
+  // ignore
+  Robot::lift()->goToPosition(Lift::aboveCubePos);
 }
 
 void bigScore(const std::shared_ptr<SideController>& controller) {
   auto&& [chassis, side] = getChassis();
 
+  // lift
   Robot::lift()->goToPosition(400);
 
   // move to goal
   speedUp();
   chassis.strafeToPoint(toClaw({{1_ft - 2.5_in, 11.5_ft + 0.5_in}, -45_deg}), makeAngle(-45_deg));
+  pros::delay(100);
 
   Robot::claw()->setState(clawStates::open);
-  pros::delay(400);
+  pros::delay(500);
   Robot::claw()->setState(clawStates::brake);
 
   // raise lift
   Robot::lift()->goToPosition(2000);
-  pros::delay(500);
+  pros::delay(100);
 
   // back up
   chassis.strafeAbsoluteDirection(1_ft, 135_deg, makeAngle(-45_deg));
