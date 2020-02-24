8070a19026aac8f35bcf5b1cf4a5f996d7efecd6
Theo Lemay <theol0403@gmail.com>
2020-02-23 22:15:01 -0800

    test stacking

--- src/7842Fmain/auton/testAuton.cpp
+++ src/7842Fmain/auton/testAuton.cpp
@@ -3,16 +3,5 @@
 void testAuton(const std::shared_ptr<SideController>& controller) {
   auto&& [chassis, side] = getChassis();
 
-  Robot::odom()->setState(mirror({0_ft, 0_ft, 0_deg}, side));
-
-  Robot::lift()->goToPosition(Lift::fourStackPos);
-
-  slowDown(); // set max voltage while lift is up
-  // drive to cube stack
-  chassis.strafeToPoint(toClaw({0_ft, 3_ft, 0_deg}), makeAngle(0_deg));
-  speedUp();
-
-  // spike stack and raise lift a bit
-  spikeCube();
-  Robot::lift()->goToPosition(Lift::aboveCubePos);
+  spikeFourStack();
 }
