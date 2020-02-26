8b81ecee3b67df5aa6b8f1b05f44b2cee93270ae
Theo Lemay <theol0403@gmail.com>
2020-02-25 21:27:44 -0800

    tune and settler

--- src/7842Fmain/auton/testAuton.cpp
+++ src/7842Fmain/auton/testAuton.cpp
@@ -3,5 +3,11 @@
 void testAuton(const std::shared_ptr<SideController>& controller) {
   auto&& [chassis, side] = getChassis();
 
-  spikeFourStack();
+  Settler::setDefaultAbort(TimeUtilFactory::withSettledUtilParams(2, 1, 1000_ms));
+
+  chassis.strafeToPoint({1_ft, 2_ft});
+  chassis.turnToAngle(90_deg);
+  chassis.strafeToPoint({0_ft, 0_ft}, makeAngle(0_deg));
+
+  // spikeFourStack();
 }
--- src/7842Fmain/config.cpp
+++ src/7842Fmain/config.cpp
@@ -32,10 +32,10 @@ void Robot::_initializeChassis() {
     _model, _odom,
     //Distance PID - To mm
     std::make_unique<IterativePosPIDController>(
-      0.016, 0.00025, 0.0003, 0, TimeUtilFactory::withSettledUtilParams(10, 10, 150_ms)),
+      0.015, 0.00025, 0.00033, 0, TimeUtilFactory::withSettledUtilParams(10, 10, 150_ms)),
     //Turn PID - To Degree
     std::make_unique<IterativePosPIDController>(
-      0.05, 0.002, 0.0006, 0, TimeUtilFactory::withSettledUtilParams(2, 2, 100_ms)),
+      0.045, 0.002, 0.0006, 0, TimeUtilFactory::withSettledUtilParams(2, 2, 100_ms)),
     //Angle PID - To Degree
     std::make_unique<IterativePosPIDController>(
       0.03, 0, 0, 0, TimeUtilFactory::withSettledUtilParams(5, 2, 100_ms)),
